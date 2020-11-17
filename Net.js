1fe6990d2cbdecc999a8d5d67c19aa8ffed19952
“Your <you@example.com>
2020-11-17 21:40:36 +0800

    first commit

---
+++ Net.js
@@ -0,0 +1,39 @@
+const host = "http://127.0.0.1:8099"; 
+
+const fetch = (ret,address,param,method='post') => {
+	console.log(param)
+	var header={
+		'content-type':'application/x-www-form-urlencoded',
+		'token':uni.getStorageSync('token')
+	}; 
+	uni.request({
+		url: host+address, //仅为示例，并非真实接口地址。
+		data: param, 
+		header:header,
+		method:method,
+		success:  (res)=> {
+		
+			
+			if(res.statusCode!=200){
+				uni.navigateTo({
+					url:'/pages/public/login'
+				})
+			}else{
+				console.log(res.data);
+				ret(res.data);
+			}
+			
+		},
+		fail: (res)=> {
+			console.log(res.data);
+			
+			uni.navigateTo({
+				url:'/pages/public/login'
+			})
+		}
+	});
+
+}
+export default {
+	fetch
+}
