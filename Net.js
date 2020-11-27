18789ec06495cb3cddffd9159413497555c8b255
shangf.li <shangf.li@sunyard.com>
2020-11-27 11:45:37 +0800

    '1'

--- Net.js
+++ Net.js
@@ -2,13 +2,13 @@ const host = "http://qiafan.applinzi.com/ThinkAdmin/public/index.php/data/";
 const getSlider = "api.data/getSlider";  //轮播
 const getCate = "api.goods/getCate";  //类型
 const getGoods = "api.goods/getGoods";  //类型
-const addOrder = "api.auth/order/add";  // 
+const addOrder = "api.auth.order/add";  // 
 
 const fetch = (ret,address,param,method='get') => {
 	console.log(param)
 	var header={
 		'content-type':'application/x-www-form-urlencoded',
-		'token':uni.getStorageSync('token')
+		'token': '921555d3ce6a3947bf7feae7f2bc719b'//uni.getStorageSync('token')
 	}; 
 	uni.request({
 		url: host+address, //仅为示例，并非真实接口地址。
--- pages/order/pay.vue
+++ pages/order/pay.vue
@@ -1,7 +1,57 @@
 <template>
+	
+	
+	<view >
+		<view class="flex">
+			
+			<navigator url="../address/address" >
+				<text> 收货地址</text>
+			</navigator>
+			
+		</view>
+		
+		<scroll-view scroll-y>
+			
+			<view class="cu-list menu">
+			<view  class="cu-item" v-for="(item,index) in order.items" :index="index">
+				
+				<text class="bg-blue"> {{item.goods_name}} </text>
+			</view>
+			
+			</view>
+			
+		</scroll-view>
+		
+		
+		
+		
+		
+		
+	</view>
+	
+	
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				banner: [],
+				goods: {},
+				sku_key: false,
+				order: {}
+			};
+		},
+		onLoad(option) {
+			var id = option.id;
+			console.log(id);
+			this.order = JSON.parse(id);
+			console.log(this.order);
+		},
+		methods: {}
+
+
+	}
 </script>
 
 <style>
